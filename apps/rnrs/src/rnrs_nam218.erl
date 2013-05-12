748891fb41feadf739766cab03c70a0da0355f4d
Martin Vejmelka <vejmelkam@gmail.com>
2013-05-11 22:25:20 -0600

    added retr application, renamed resolver_xxxx to rnrs_xxxx file convention

--- apps/rnrs/src/rnrs_nam218.erl
+++ apps/rnrs/src/rnrs_nam218.erl
@@ -1,13 +1,15 @@
 
 %
 %  A resolver computes storage ids and URLs for GRIB files.
+%  Due to operational constraints, it may happen that current
+%  cycle run files are unavailable, in this case, older cycle
+%  run files may be requested using CycleRunDelta > 0 parameter.
 %
 %  This resolver computes a manifest for the NAM grid #218
 %  84-hr forecast source.
 %
 
-
--module(resolver_nam218).
+-module(rnrs_nam218).
 -author("Martin Vejmelka <vejmelkam@gmail.com").
 -export([storage_prefix/0, url_prefix/0, manifest/2]).
 
@@ -20,5 +22,5 @@ url_prefix() ->
     "http://nomads.ncep.noaa.gov/pub/data/nccf/com/nam/prod/".
 
 
-manifest(From, To) ->
+manifest(_From, _To, _CycleRunDelta) ->
     [].
