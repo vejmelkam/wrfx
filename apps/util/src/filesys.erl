cda1148fd6f7a74e744462fc379ca06170f44bf2
Martin Vejmelka <vejmelkam@gmail.com>
2013-05-14 11:23:41 -0600

    added filesys module for file system related utility functions

---
+++ apps/util/src/filesys.erl
@@ -0,0 +1,21 @@
+
+
+-module(filesys).
+-author("Martin Vejmelka <vejmelkam@gmail.com").
+-export([list_files_regexp/2]).
+
+
+list_files_regexp(Dir, RegExp) ->
+    {ok, MP} = re:compile(RegExp),
+    {ok, FN} = file:list_dir(Dir),
+    lists:filter(fun (X) -> is_match_re(X, MP) end, FN).
+
+
+is_match_re(X, MP) ->
+    case re:run(X, MP) of
+	{match, _} ->
+	    true;
+	nomatch ->
+	    false
+    end.
+
