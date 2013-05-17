73459678f153b57bc3efcba37b29d66bc11e1b76
Martin Vejmelka <vejmelkam@gmail.com>
2013-05-17 17:17:38 -0600

    added external process execution/monitoring and file monitoring in xmon

---
+++ apps/xmon/src/exmon.erl
@@ -0,0 +1,76 @@
+
+%
+%  Execution and monitoring module for external processes.
+%  Uses open_port/2 to establish communication with an external
+%  program.
+%
+
+
+-module(exmon).
+-author("Martin Vejmelka <vejmelkam@gmail.com>").
+-export([run/2, run/3, kill/1]).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+
+kill(PID) ->
+    PID ! send_kill.
+
+
+run(ExFile, Args, Monitors) ->
+    spawn(fun() -> run_and_monitor(ExFile, Args, Monitors) end).
+
+run(ExFile, Monitors) ->
+    spawn(fun() -> run_and_monitor(ExFile, [], Monitors) end).
+
+
+run_and_monitor(ExFile, Args, Monitors) ->
+    P = open_port({spawn_executable, ExFile}, [{args, Args}, {line, 5000}, exit_status, use_stdio]),
+    monitor_process(P, Monitors).
+    
+
+monitor_process(P, Monitors) ->
+    receive
+	send_kill ->
+	    {os_pid, OsPid} = erlang:port_info(P, os_pid),
+	    os:cmd(io_lib:format("kill -9 ~p", [OsPid])),
+	    monitor_process(P, Monitors);
+	{_From, {data, {eol, Line}}} ->
+	    router:multicast({line, Line}, Monitors),
+	    monitor_process(P, Monitors);
+	{_From, {exit_status, S}} ->
+	    router:multicast({exit_detected, S}, Monitors);
+	M ->
+	    io:format("exmon: unexpected message ~p~n", [M]),
+	    monitor_process(P, Monitors)
+    end.
+
+
+
+-ifdef(TEST).
+
+simple_test() ->
+    run("/bin/echo", ["test_string"], [self()]),
+    receive
+	Msg ->
+	    ?assert(Msg =:= {line, "test_string"})
+    end,
+    receive
+	Msg2 ->
+	    ?assert(Msg2 =:= {exit_detected, 0})
+    end.
+
+
+kill_test() ->
+    PID = run("/bin/sleep", ["5"], [self()]),
+    kill(PID),
+    receive
+	Msg ->
+	    ?assert(element(1, Msg) =:= exit_detected)
+    end.
+
+
+-endif.
+    
---
+++ apps/xmon/src/fmon.erl
@@ -0,0 +1,91 @@
+
+
+%
+%
+%  A file monitoring service that watches a file and sends
+%  out new available data (whenever an entire line is available)
+%  to registered Monitors.
+%
+%
+
+
+-module(fmon).
+-author("Martin Vejmelka <vejmelkam@gmail.com>").
+-export([start/2, stop/1]).
+
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+
+start(F, Monitors) ->
+    spawn(fun () -> start_monitor(F, Monitors) end).
+
+
+stop(PID) ->
+    PID ! stop.
+
+
+start_monitor(F, Monitors) ->
+    wait_for_file(continue, F, Monitors).
+
+
+wait_for_file(continue, F, M) ->
+    case filelib:is_file(F) of
+	true ->
+	    {ok, D} = file:open(F, [read]),
+	    try_read_line(continue, D, M);
+	false ->
+	    wait_for_file(check_for_messages(M), F, M)
+    end;
+wait_for_file(stop, _F, _M) ->
+    ok.
+
+
+try_read_line(continue, D, M) ->
+    case file:read_line(D) of
+	eof ->
+	    try_read_line(check_for_messages(M), D, M);
+	{ok, L} ->
+	    router:multicast({line, L}, M),
+	    try_read_line(continue, D, M);
+	{error, R} ->
+	    router:multicast({error, R}, M),
+	    file:close(D)
+    end;
+try_read_line(stop, D, _M) ->
+    file:close(D).
+
+
+check_for_messages(M) ->
+    receive
+	stop ->
+	    router:multicast({stopped, self()}, M),
+	    stop;
+	Msg ->
+	    io:format("fmon: unexpected message [~p]~n", [Msg]),
+	    continue
+    after
+	500 ->
+	    continue
+    end.
+	
+    
+
+-ifdef(TEST).
+
+simple_test() ->
+    PID = start("/etc/passwd", [self()]),
+    stop(PID),
+    read_all_in(PID).
+
+read_all_in(PID) ->
+    receive
+	{line, _L} ->
+	    read_all_in(PID);
+	{stopped, PID} ->
+	    ok
+    end.
+    
+-endif.
