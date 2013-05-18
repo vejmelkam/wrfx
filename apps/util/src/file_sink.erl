bd5c3cf3b2881fbb3afc7029c479d949582489a1
Martin Vejmelka <vejmelkam@gmail.com>
2013-05-17 18:14:52 -0600

    added file_sink for asynchronous writing to files from executing processes

---
+++ apps/util/src/file_sink.erl
@@ -0,0 +1,31 @@
+
+
+%
+%
+%
+
+
+-module(file_sink).
+-author("Martin Vejmelka <vejmelkam@gmail.com>").
+-export([start/1, stop/1]).
+
+
+
+start(F) ->
+    {ok, D} = file:open(F, [write]),
+    spawn(fun () -> file_sink_loop(D) end).
+
+
+stop(PID) ->
+    PID ! {eof, self()}.
+
+
+file_sink_loop(D) ->
+    receive
+	{line, L} ->
+	    file:write(D, L),
+	    file:write(D, "\n"),
+	    file_sink_loop(D);
+	{eof, _From} ->
+	    file:close(D)
+    end.
