195cda73b6c5dd5db779913d8f9254e0682a34ee
Martin Vejmelka <vejmelkam@gmail.com>
2013-05-28 20:34:26 -0600

    added new app wrfx to run system

---
+++ apps/wrfx/src/wrfx_app.erl
@@ -0,0 +1,16 @@
+-module(wrfx_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    wrfx_sup:start_link().
+
+stop(_State) ->
+    ok.
